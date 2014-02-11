f9c62fc73794b2ee4e7e32ecf5805b0be322d80e
vagrant <duy.phan@asnet.com.vn>
2014-02-11 08:33:27 +0000

    first commit with ribbit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ribbit::Application.config.secret_key_base = 'd08f9388697ff534d8a73234d1e42001d1abc6d6526a0228948c6b9d9120465448371a6333cc409315df2ec557dc36be0c0dbe61d9cbfac6f6f8261950f5fb0f'
